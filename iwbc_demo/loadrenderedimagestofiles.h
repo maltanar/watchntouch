745e4259e47cec42bb8b8c5f996337bc0084843e
utkusirin <utkusirin@5c9df1b0-2a2a-0b20-746c-15ce77988770>
2011-03-19 08:01:36 +0000

    issue #3, loadrenderedimagestofiles.cpp and .h files are forgetten to commit in r60. These files are added. Sorry for inconsistency.

---
+++ iwbc_demo/loadrenderedimagestofiles.h
@@ -0,0 +1,27 @@
+#ifndef LOADRENDEREDIMAGESTOFILES_H
+#define LOADRENDEREDIMAGESTOFILES_H
+
+#include <QThread>
+#include <poppler-qt4.h>
+
+#include "appglobals.h"
+
+class LoadRenderedImagesToFiles : public QThread
+{
+    Q_OBJECT
+public:
+    explicit LoadRenderedImagesToFiles(QObject *parent = 0);
+    void run(); // From QThread
+
+    int slideCount;
+    Poppler::Document *doc;
+    QString fileName;
+
+signals:
+    void imagesAreReady();
+
+public slots:
+
+};
+
+#endif // LOADRENDEREDIMAGESTOFILES_H
