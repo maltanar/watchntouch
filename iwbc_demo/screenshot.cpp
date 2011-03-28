7cc738ceab75309538480cdc4476efccdeb6933f
maltanar <maltanar@5c9df1b0-2a2a-0b20-746c-15ce77988770>
2011-03-28 13:12:38 +0000

    hide screenshot button while capturing screen

--- iwbc_demo/screenshot.cpp
+++ iwbc_demo/screenshot.cpp
@@ -1,11 +1,16 @@
 #include "screenshot.h"
 #include "ui_screenshot.h"
 
+#include <QDesktopWidget>
+
 Screenshot::Screenshot(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Screenshot)
 {
     ui->setupUi(this);
+    setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint);
+    QSize ds = qApp->desktop()->size();
+    move(ds.width()-width(), ds.height()-height());
 }
 
 Screenshot::~Screenshot()
@@ -15,5 +20,7 @@ Screenshot::~Screenshot()
 
 void Screenshot::on_scrnshot_clicked()
 {
+    hide();
+    sleep(1);
     emit goBack();
 }
